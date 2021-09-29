817688cb6c26bf80201061a0434863dcf385fe5b
LebedevTD <69440244+LebedevTD@users.noreply.github.com>
2021-09-29 14:12:47 +0300

    Add files via upload

---
+++ Logistic Regression/Plot ROC curves.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+import matplotlib
+
+os.chdir('C:\\Lab\\Python\\NB\\GitHub\\')
+#os.chdir('C:\\Work_dir\\')
+
+"""Select datasets with calculated scores and known outcomes,
+    choose colors for each dataset"""
+datasets= {'SEQC': ['SEQC_scores.xlsx', '#323232'],
+           'Versteeg': ['Versteeg_scores.xlsx', '#5baf06'],
+           'NRC': ['NRC_scores.xlsx', '#dddd73'],
+           'Westermann': ['Westermann_scores.xlsx','#790079'],
+           }
+matplotlib.rcParams['axes.linewidth']= 3
+fig, ax = plt.subplots(figsize=(10, 10))
+for data in datasets.keys():
+    df= pd.read_excel(datasets[data][0])
+    df= df[df['#event_overall']!='na']
+    df['event']= df['#event_overall'].apply(lambda x: 1 if x== 'yes' else 0)
+    df1=df
+    event= df1['event']
+    log_p= df1['p']
+    # calculate ROC
+    ns_p = [0 for _ in range(len(event))]
+    ns_auc = roc_auc_score(event, ns_p)
+    log_auc = roc_auc_score(event, log_p)
+    ns_fpr, ns_tpr, _ = roc_curve(event, ns_p)
+    lr_fpr, lr_tpr, _ = roc_curve(event, log_p)
+    # plot AUC values
+    ax.plot(lr_fpr, lr_tpr, marker='.', linewidth=6, color=datasets[data][1], label=data+' AUC= %.2f' % log_auc)
+ax.plot(ns_fpr, ns_tpr, linestyle='--', linewidth=6, color='#4b4b4b')
+ax.set_xlabel('False Positive Rate', fontsize=24, weight='bold')
+ax.set_ylabel('True Positive Rate', rotation=90, fontsize=24, weight='bold')
+ax.tick_params(labelsize=18)
+# show the legend
+plt.legend(prop={'size': 24}, loc=(0.32, 0.0))
+#plt.legend(prop={'size': 24}, loc=(0.36, -0.5))
+plt.savefig('LogReg_datasets_ROC_AUC.png', bbox_inches = 'tight')
---
+++ Logistic Regression/Predict outcome.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import os
+import math
+
+def predict_survival (df):
+    """Calculates event probability (death) based on gene expression from input dataframe df
+    and precalculated models (nomycn_model, and mycn_model)"""
+    p_list= list()
+    for ind1, row1 in df.iterrows():
+        """probability for mycn amplified tumors"""
+        if df.at[ind1, '#mycn_status']=='yes':
+            score=0.075 #intercept parameter
+            for ind2, row2 in mycn_model.iterrows():
+                gene= mycn_model.at[ind2, 'Gene']
+                if gene in df.columns:
+                    score= score+ mycn_model.at[ind2,'coef']*df.at[ind1, gene]
+            p= 1/(1+math.exp(-score))
+        else: 
+            """probability for mycn non-amplified tumors"""
+            score=0.523 #intercept parameter
+            for ind2, row2 in nomycn_model.iterrows():
+                gene= nomycn_model.at[ind2, 'Gene']
+                if gene in df.columns:
+                    score= score+ nomycn_model.at[ind2,'coef']*df.at[ind1, gene]
+            p= 1/(1+math.exp(-score))
+        p_list.append(p)
+    df['p']=p_list
+    df['group']=''
+    """assing risk groups based on predicted probabilities"""
+    for ind, row in df.iterrows():
+        if df.at[ind, 'p']>=0.6:
+            df.at[ind, 'group']=5
+        elif df.at[ind, 'p']>=0.4:
+            df.at[ind, 'group']=4
+        elif df.at[ind, 'p']>=0.15:
+            df.at[ind, 'group']=3
+        elif df.at[ind, 'p']>=0.05:
+            df.at[ind, 'group']=2
+        else:
+            df.at[ind, 'group']=1
+    """returns df with calculated event probabilities and risk groups"""
+    return df 
+
+"""Set work folder with input files"""
+os.chdir('C:\\Lab\\Python\\NB\\GitHub\\')
+#os.chdir('C:\\Work_dir\\')
+"""list of datasets for survival prediction"""
+datasets= {'Cangelosi': 'Tumor Neuroblastoma integrated platforms - Cangelosi - 786_GFgenes_norm.xlsx',
+           'NRC': 'NRC_GFgenes_norm.xlsx',
+           'Westermann': 'Tumor Neuroblastoma ALT - Westermann - 144 - tpm - gencode19_GFgenes_norm.xlsx',
+           'Versteeg': 'Versteeg_GFgenes_norm.xlsx',
+           'SEQC': 'SEQC RPM_GFgenes_norm.xlsx'}
+nomycn_model= pd.read_excel('LogReg model MYCN_no.xlsx') #load model parameters for mycn non-amplified tumors
+mycn_model= pd.read_excel('LogReg model MYCN_yes.xlsx') #load model parameters for mycn amplified tumors
+"""calculates survival probability for each dataset"""
+for dataset in list(datasets.keys()):
+    data=pd.read_excel(datasets[dataset])
+    result= predict_survival(data)
+    result.to_excel(dataset+'_scores.xlsx') #saves results to excel file
+
+
+        
